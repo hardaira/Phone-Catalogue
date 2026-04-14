a171b9f470d5eba579f8cf7193ff6c022076bb0d
hardaira <hardaira@yahoo.com>
2026-04-14 10:00:32 +0200

    version for portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+import { Navbar } from './components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './components/Footer/Footer';
+export const App = () => (
+  <div className="page">
+    <Navbar />
+    <div>
+      <Outlet />
+    </div> 
+    <Footer />
+  </div>
+);
